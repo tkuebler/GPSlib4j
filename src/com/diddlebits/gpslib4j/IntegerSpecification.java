e7aa70b5d9ee1fab0c374473de9513cbdac7454c
pvalsecc <pvalsecc>
2005-11-10 20:11:52 +0000

    Added some utility methods

--- src/com/diddlebits/gpslib4j/IntegerSpecification.java
+++ src/com/diddlebits/gpslib4j/IntegerSpecification.java
@@ -14,9 +14,9 @@ public class IntegerSpecification {
     }
 
     public IntegerSpecification(GPSEnumDefinition definition) {
-        min=definition.getMinValue();
-        max=definition.getMaxValue();
-        canBeNull=definition.isCanBeNull();
+        min = definition.getMinValue();
+        max = definition.getMaxValue();
+        canBeNull = definition.isCanBeNull();
     }
 
     public boolean isCanBeNull() {
@@ -32,40 +32,52 @@ public class IntegerSpecification {
     }
 
     public void warningIfInvalid(String fieldName, long value) {
-        String result=checkSyntax(value);
-        if(result!=null) {
+        String result = checkSyntax(value);
+        if (result != null) {
             System.out.println("Invalid integer content for field " + fieldName
                     + ": " + result);
         }
     }
 
     private String checkSyntax(long value) {
-        if(value<min) {
-            return "Out of range ("+value+"<"+min+")";
-        } else if(value>max) {
-            return "Out of range ("+value+">"+max+")";
+        if (value < min) {
+            return "Out of range (" + value + "<" + min + ")";
+        } else if (value > max) {
+            return "Out of range (" + value + ">" + max + ")";
         }
         return null;
     }
 
-    public void throwIfInvalid(String name, long value) throws InvalidFieldValue {
-        String result=checkSyntax(value);
-        if (result!=null) {
-            throw new InvalidFieldValue(name, Long
-                    .toString(value), result);
+    public void throwIfInvalid(String name, long value)
+            throws InvalidFieldValue {
+        String result = checkSyntax(value);
+        if (result != null) {
+            throw new InvalidFieldValue(name, Long.toString(value), result);
         }
     }
-    
+
     public int getMaxStringLength() {
         int result;
-        if(Math.abs(min)<Math.abs(max))
-            result=(int)Math.floor(Math.log(Math.abs(min))/Math.log(10))+1;
+        if (Math.abs(min) < Math.abs(max))
+            result = (int) Math.floor(Math.log(Math.abs(min)) / Math.log(10)) + 1;
         else
-            result=(int)Math.floor(Math.log(Math.abs(max))/Math.log(10))+1;
-            
-        if(min<0) {
-            result+=1;
+            result = (int) Math.floor(Math.log(Math.abs(max)) / Math.log(10)) + 1;
+
+        if (min < 0) {
+            result += 1;
         }
         return result;
+    }
+
+    public long convertFromString(String value) throws InvalidFieldValue {
+        short tmp;
+        try {
+            tmp = Short.parseShort(value);
+        } catch (NumberFormatException e) {
+            throw new InvalidFieldValue(GPSFields.IndexField, value, e
+                    .getMessage());
+        }
+        throwIfInvalid(GPSFields.IndexField, tmp);
+        return tmp;
     }
 }
